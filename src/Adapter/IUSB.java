35ded23c8ffb146888115375e91046ef0007400f
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-09-21 08:50:48 -0300

    adapter

---
+++ src/Adapter/IUSB.java
@@ -0,0 +1,6 @@
+package Adapter;
+
+public interface IUSB {
+    void plugIn();
+    void transferData();
+}
---
+++ src/Adapter/USBApple.java
@@ -0,0 +1,10 @@
+package Adapter;
+
+public class USBApple {
+    void connect(){
+        System.out.println("Connecting Apple device...");
+    }
+    void transferData(){
+        System.out.println("Transferring data using Apple device");
+    }
+}
